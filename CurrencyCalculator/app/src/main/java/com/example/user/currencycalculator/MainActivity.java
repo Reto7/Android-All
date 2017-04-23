90999ebb89ff6fd58e7c40e3c2ea0bfaf0931f29
Reto Kaufmann <reto.kaufmann@adon.li>
2017-04-23 20:44:43 +0200

    .

--- CurrencyCalculator/app/src/main/java/com/example/user/currencycalculator/MainActivity.java
+++ CurrencyCalculator/app/src/main/java/com/example/user/currencycalculator/MainActivity.java
@@ -5,6 +5,18 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+        Am Schluss eine Übung begonnen (Hausaufgabe)
+        Neues Projekt: CurrencyCalculator
+        Immer fix von CHF ausgehend
+        1. Eingabefeld (entweder eine Dropdownliste oder Freitext):
+        Zielwährung (USD,EUR,GBP)
+        Falls Freitext dann mit Exceptionhandling (falls man eine falsche Wgh. eingibt)
+        2. Eingabefeld
+        den umzurechnenden Betrag
+        3. Button und Ausgabefeld, z.B. "10 CHF sind umgerechnet 12839.- USD"
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
