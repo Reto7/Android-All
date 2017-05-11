0d0661c09d493413823087ba51428e0e89ed0e7a
Reto Kaufmann <reto.kaufmann@adon.li>
2017-05-11 21:02:46 +0200

    ok

--- KontaktListeMitDetail/app/src/main/java/ch/ibw/reto/kontaktlistemitdetail/DetailActivity.java
+++ KontaktListeMitDetail/app/src/main/java/ch/ibw/reto/kontaktlistemitdetail/DetailActivity.java
@@ -1,10 +1,16 @@
 package ch.ibw.reto.kontaktlistemitdetail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.net.URI;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,7 +26,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         // Parameter auslesen
         Bundle extras = getIntent().getExtras();
-        Kontakt kontakt = (Kontakt) extras.getSerializable("KONTAKT");
+        final Kontakt kontakt = (Kontakt) extras.getSerializable("KONTAKT");
 
         Log.i(PROG, "Kontakt: " +kontakt);
 
@@ -29,10 +35,37 @@ public class DetailActivity extends AppCompatActivity {
         anzeigefeld_kontaktname.setText(kontakt.getName());
         //
         anzeigefeld_kontakttel = (TextView) findViewById(R.id.anzeigefeld_kontakttel);
-        anzeigefeld_kontakttel.setText(kontakt.getName());
+        anzeigefeld_kontakttel.setText(kontakt.getTelefonnummer());
         //
         anzeigefeld_kontaktweb = (TextView) findViewById(R.id.anzeigefeld_kontaktweb);
-        anzeigefeld_kontaktweb.setText(kontakt.getName());
+        anzeigefeld_kontaktweb.setText(kontakt.getWebseite());
+
+
+        // KLICK
+        anzeigefeld_kontakttel.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Log.i(PROG, "Tel geklickt ...");
+                   Intent intent1 = new Intent(Intent.ACTION_DIAL,
+                        Uri.parse("tel:"+kontakt.getTelefonnummer() ));
+                   startActivity(intent1);
+               }
+           }
+        );
+
+        // KLICK
+        anzeigefeld_kontaktweb.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Log.i(PROG, "Web geklickt ...");
+                  Intent intent2 = new Intent(Intent.ACTION_VIEW,
+                          Uri.parse(kontakt.getWebseite() ));
+                  startActivity(intent2);
+              }
+          }
+        );
+
+
 
 
     }
