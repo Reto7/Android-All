280cb55dc8589f3036c0148b3eea305397c53ae4
Reto Kaufmann <reto.kaufmann@adon.li>
2017-05-17 22:03:15 +0200

    cam

--- GoogleMapsDashCam/app/src/main/java/com/test/reto/googlemapsdashcam/MapsActivity.java
+++ GoogleMapsDashCam/app/src/main/java/com/test/reto/googlemapsdashcam/MapsActivity.java
@@ -1,23 +1,39 @@
 package com.test.reto.googlemapsdashcam;
 
-import android.support.v4.app.FragmentActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     public static final String PROG = "____MapsDashCam";
 
     private GoogleMap mMap;
+
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Bitmap mImageBitmap;
+    private String mCurrentPhotoPath;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onClick(View v) {
                 Log.w(PROG, "Start Cam clicked...");
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+                    // Create the File where the photo should go
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException ex) {
+                        // Error occurred while creating the File
+                        Log.i(PROG, "IOException");
+                    }
+                    // Continue only if the File was successfully created
+                    if (photoFile != null) {
+                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+                        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+                    }
+                }
             }
         });
 
@@ -46,6 +78,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Log.w(PROG, "Stop Cam clicked...");
             }
         });
+
+        // get the iMAGe view           .... https://stackoverflow.com/questions/5991319/capture-image-from-camera-and-display-in-activity
+        this.imageView = (ImageView)this.findViewById(R.id.ImageView);
     }
 
 
@@ -69,5 +104,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         */
+    }
+
+
+
+
+    private File createImageFile() throws IOException {
+        Log.w(PROG, "createImageFile...");
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        Log.w(PROG, "timeStamp:"+timeStamp);
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        Log.w(PROG, "storageDir:"+storageDir.toString());
+        File image = File.createTempFile(
+                imageFileName,  // prefix
+                ".jpg",         // suffix
+                storageDir      // directory
+        );
+        Log.w(PROG, "storageDir:"+image.toString());
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        Log.w(PROG, "mCurrentPhotoPath:"+mCurrentPhotoPath.toString());
+        return image;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
+                mImageView.setImageBitmap(mImageBitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
