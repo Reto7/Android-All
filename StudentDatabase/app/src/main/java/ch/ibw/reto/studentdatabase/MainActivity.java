a623cb0b394f63f4eb3eda96b9dbd66a2902d250
Reto Kaufmann <reto.kaufmann@adon.li>
2017-06-01 19:05:45 +0200

    spinner ok

--- StudentDatabase/app/src/main/java/ch/ibw/reto/studentdatabase/MainActivity.java
+++ StudentDatabase/app/src/main/java/ch/ibw/reto/studentdatabase/MainActivity.java
@@ -2,12 +2,32 @@ package ch.ibw.reto.studentdatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editStudent;
+    private Spinner spinnerStudienrichtung;
+  //  private TextView textAusgabe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the values from input field
+        editStudent = (EditText) findViewById(R.id.edit_student);
+        spinnerStudienrichtung = (Spinner) findViewById(R.id.spinner_studienrichtung);
+
+
+        // liste in spinner
+        // spinner dropdownliste mit werten befuellen (aus Currency Enumeration)
+        ArrayAdapter<Studienrichtung> adapter = new ArrayAdapter<Studienrichtung>(MainActivity.this,
+                android.R.layout.simple_spinner_item, Studienrichtung.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerStudienrichtung.setAdapter(adapter);
     }
 }
---
+++ StudentDatabase/app/src/main/java/ch/ibw/reto/studentdatabase/Studienrichtung.java
@@ -0,0 +1,13 @@
+package ch.ibw.reto.studentdatabase;
+
+import java.util.HashMap;
+
+/**
+ * Created by user on 23.04.2017.
+ */
+
+public enum Studienrichtung {
+    Applikationsentwicklung, Architektur, Friseurwesen, Jura
+
+
+}
