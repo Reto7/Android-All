ed91a7202746360ff427729d3264e1fbf9f53ec4
Reto Kaufmann <reto.kaufmann@adon.li>
2017-06-15 19:27:22 +0200

    basis

--- GeoLocation/app/src/main/java/test/reto/geolocation/MainActivity.java
+++ GeoLocation/app/src/main/java/test/reto/geolocation/MainActivity.java
@@ -1,13 +1,41 @@
 package test.reto.geolocation;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String PROG = "____GEOLOCATION";
+    TextView textViewShow;
+    Button buttonGet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        textViewShow = (TextView) findViewById(R.id.showText);
+        buttonGet = (Button) findViewById(R.id.buttonGet);
+
+        buttonGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+                startCapturingLocation();
+            }
+        });
+    }
+
+    private void startCapturingLocation() {
+        Log.w(PROG, "start capturing location ...");
+
     }
 }
