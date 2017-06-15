c5add8fd25b1fb3a834c60acd213466c8e5ae9bf
Reto Kaufmann <reto.kaufmann@adon.li>
2017-06-15 20:23:57 +0200

    jetzt funktionierts! im Manifest muss es auf der obersten XML Ebene sein, nicht innerhalb Application!

--- GeoLocation/app/src/main/java/test/reto/geolocation/MainActivity.java
+++ GeoLocation/app/src/main/java/test/reto/geolocation/MainActivity.java
@@ -1,12 +1,11 @@
 package test.reto.geolocation;
 
-import android.content.ContentValues;
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -14,9 +13,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
     // app-defined int constant. The callback method gets the
     // result of the request.
-    private static final int MY_PERMISSION_ACCESS_COURSE_LOCATION = 1;
+    private static final int MY_PERMISSION_ACCESS_FINE_LOCATION = 777;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,19 +46,30 @@ public class MainActivity extends AppCompatActivity {
     private void startCapturingLocation() {
         Log.w(PROG, "start capturing location ...");
 
-        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
-
-            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },
-                   MY_PERMISSION_ACCESS_COURSE_LOCATION );
+        if ( ContextCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+            Log.w(PROG, "not granted");
+            ActivityCompat.requestPermissions( this, new String[] {  Manifest.permission.ACCESS_FINE_LOCATION  },
+                    MY_PERMISSION_ACCESS_FINE_LOCATION);
+            Log.w(PROG, "request permission fertig");
+        } else {
+            Log.w(PROG, "granted");
         }
 
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Log.w(PROG, "locationmanager : " +locationManager);
         locationManager.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER, 0L, 0.0F,
+                LocationManager.GPS_PROVIDER, 0L, 0.0F,
                 new LocationListener() {
                     @Override
                     public void onLocationChanged(Location location) {
-
+                        Log.w(PROG, "location changed !");
+                        Log.w(PROG, "location long : " +location.getLongitude());
+                        String message = String.format(
+                                "Location captured:%nLON %s / LAT %s",
+                                location.getLongitude(),
+                                location.getLatitude()
+                        );
+                        textViewShow.setText(message);
                     }
 
                     @Override
@@ -87,9 +95,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        System.out.println("1");
         switch (requestCode) {
-            case MY_PERMISSION_ACCESS_COURSE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
+            case MY_PERMISSION_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays arACCESS_FINE_LOCATIONe empty.
+                System.out.println("2");
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
